4ed23fa4ba8331160a4efe493db2f49057163a02
Richard Top <richard.topouchian@uib.no>
2023-03-14 12:50:54 +0000

    fixing code

--- EESSI-pilot-install-software.sh
+++ EESSI-pilot-install-software.sh
@@ -117,7 +117,7 @@ fi
 if [ $DELETE -eq 1 ]; then
 
         echo "Deleting the generic REPOs..."
-        rm -rf /cvmfs/pilot.nessi.no/latest/software/linux/x86_64/generic/modules/*
+        rm -rf /cvmfs/pilot.nessi.no/latest/software/linux/x86_64/generic/modules/all/*
         rm -rf /cvmfs/pilot.nessi.no/latest/software/linux/x86_64/generic/software/*
 fi
 
