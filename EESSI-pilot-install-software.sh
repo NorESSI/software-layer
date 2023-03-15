ec378bfc564ef1d8a7bd454bc207fbfe8a231165
Thomas Roeblitz <thomas.roblitz@uib.no>
2023-03-15 23:57:01 +0100

    add Java-17 and tweak versions

--- EESSI-pilot-install-software.sh
+++ EESSI-pilot-install-software.sh
@@ -240,6 +240,14 @@ fail_msg="Failed to install Java, woopsie..."
 $EB Java-11.eb --robot --include-easyblocks-from-pr 2557
 check_exit_code $? "${ok_msg}" "${fail_msg}"
 
+# install Java with fixed custom easyblock that uses patchelf to ensure right glibc is picked up,
+# see https://github.com/EESSI/software-layer/issues/123
+# and https://github.com/easybuilders/easybuild-easyblocks/pull/2557
+ok_msg="Java installed, off to a good (?) start!"
+fail_msg="Failed to install Java, woopsie..."
+$EB Java-17.eb --robot --include-easyblocks-from-pr 2557
+check_exit_code $? "${ok_msg}" "${fail_msg}"
+
 # install GCC for foss/2020a
 export GCC_EC="GCC-9.3.0.eb"
 echo ">> Starting slow with ${GCC_EC}..."
@@ -247,17 +255,15 @@ ok_msg="${GCC_EC} installed, yippy! Off to a good start..."
 fail_msg="Installation of ${GCC_EC} failed!"
 # pull in easyconfig from https://github.com/easybuilders/easybuild-easyconfigs/pull/14453,
 # which includes patch to fix build of GCC 9.3 when recent kernel headers are in place
-$EB ${GCC_EC} --robot --from-pr 14453 GCCcore-9.3.0.eb
+$EB ${GCC_EC} --robot
 check_exit_code $? "${ok_msg}" "${fail_msg}"
 
 # install GCC for foss/2021a
 export GCC_EC="GCC-10.3.0.eb"
-echo ">> Starting slow with ${GCC_EC}..."
-ok_msg="${GCC_EC} installed, yippy! Off to a good start..."
+echo ">> Continuing with ${GCC_EC}..."
+ok_msg="${GCC_EC} installed too, yippy! Off to a good start..."
 fail_msg="Installation of ${GCC_EC} failed!"
-# pull in easyconfig from https://github.com/easybuilders/easybuild-easyconfigs/pull/14453,
-# which includes patch to fix build of GCC 9.3 when recent kernel headers are in place
-$EB ${GCC_EC} --robot --from-pr 14453 GCCcore-10.3.0.eb
+$EB ${GCC_EC} --robot
 check_exit_code $? "${ok_msg}" "${fail_msg}"
 
 # install CMake with custom easyblock that patches CMake when --sysroot is used
