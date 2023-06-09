6b1831b50ed7cd02f3b1a0acefc2bcd0ad3c50ab
Richard Top <richard.topouchian@uib.no>
2023-06-09 10:27:04 +0000

    add C.utf8

--- EESSI-pilot-install-software.sh
+++ EESSI-pilot-install-software.sh
@@ -226,12 +226,7 @@ if [[ $GENERIC -eq 1 ]]; then
 fi
 eb_install_easystack_out=${TMPDIR}/eb_install_easystack.out
 ${EB:-eb} --robot --easystack eessi-${EESSI_PILOT_VERSION}.yml --experimental | tee ${eb_install_easystack_out}
-echo "***********************************************"
-locale -a
 export LANG=C.utf8
-locale 
-eb -r $TOPDIR/IPython-7.25-Modified.eb
-echo "***********************************************"
 echo ">> Creating/updating Lmod cache on $(date) (nr 1) ..."
 export LMOD_RC="${EASYBUILD_INSTALLPATH}/.lmod/lmodrc.lua"
 if [ ! -f $LMOD_RC ]; then
