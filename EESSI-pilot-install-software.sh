daa2b5812e1a4fb3289ee4cf7bdb2930c95cc37f
Richard Top <richard.topouchian@uib.no>
2023-06-09 08:32:12 +0000

    adde LANG C

--- EESSI-pilot-install-software.sh
+++ EESSI-pilot-install-software.sh
@@ -227,7 +227,7 @@ fi
 eb_install_easystack_out=${TMPDIR}/eb_install_easystack.out
 ${EB:-eb} --robot --easystack eessi-${EESSI_PILOT_VERSION}.yml --experimental | tee ${eb_install_easystack_out}
 echo "***********************************************"
-export LANG=en_US.utf8
+export LANG=C
 locale 
 eb -r $TOPDIR/IPython-7.25-Modified.eb
 echo "***********************************************"
