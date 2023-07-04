67965fb675bbedb7a390dc598ec53cdff4cc30d7
Richard Top <richard.topouchian@uib.no>
2023-07-04 15:17:56 +0000

    {2023.06}[foss/2021a] Qt5 V5.15.2

--- eb_hooks.py
+++ eb_hooks.py
@@ -30,7 +30,6 @@ def get_eessi_envvar(eessi_envvar):
 
     return eessi_envvar_value
 
-
 def get_rpath_override_dirs(software_name):
     # determine path to installations in software layer via $EESSI_SOFTWARE_PATH
     eessi_software_path = get_eessi_envvar('EESSI_SOFTWARE_PATH')
@@ -59,8 +58,16 @@ def parse_hook(ec, *args, **kwargs):
     # determine path to Prefix installation in compat layer via $EPREFIX
     eprefix = get_eessi_envvar('EPREFIX')
 
+#Disable Qt5/5.15.2 qtwebengine in Sanity-check
+    Qt5_check_qtwebengine_disable(ec, eprefix)
+
     if ec.name in PARSE_HOOKS:
         PARSE_HOOKS[ec.name](ec, eprefix)
+
+def Qt5_check_qtwebengine_disable(ec, eprefix):
+    if ec.name == 'Qt5'and ec.version == '5.15.2':
+         ec['check_qtwebengine'] = False  
+         print_msg(NOTE:the value of check_qtwebengine has been set to %s , ec['check_qtwebengine'])
 
 
 def pre_prepare_hook(self, *args, **kwargs):
@@ -261,3 +268,13 @@ PRE_CONFIGURE_HOOKS = {
     'OpenBLAS': pre_configure_hook_openblas_optarch_generic,
     'WRF': pre_configure_hook_wrf_aarch64,
 }
+#Disable Qt5/5.15.2 qtwebengine in Sanity-check
+    Qt5_check_qtwebengine_disable(ec, eprefix)
+
+    if ec.name in PARSE_HOOKS:
+        PARSE_HOOKS[ec.name](ec, eprefix)
+
+def Qt5_check_qtwebengine_disable(ec, eprefix):
+    if ec.name == 'Qt5'and ec.version == '5.15.2':
+         ec['check_qtwebengine'] = False  
+         print_msg(NOTE:the value of check_qtwebengine has been set to %s , ec['check_qtwebengine'])
